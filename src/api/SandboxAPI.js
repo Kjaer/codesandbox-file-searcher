1dec031bf42bef64f215ac9b38480e487f03874d
Kjaer <kayer.halil@gmail.com>
2020-12-17 18:43:28 +0100

    better validation for non and less than 3 character search and using mock endpoint in order to avoid CORS

--- src/api/SandboxAPI.js
+++ src/api/SandboxAPI.js
@@ -1,20 +1,13 @@
 // ToDo: change this with real fetch from endpoint
 
-import sandboxDummy from "./sandbox.json";
+// const API_URL = 'https://codesandbox.io/api/v1/sandboxes/84jkx';
+const MOCK_API_URL = 'https://run.mocky.io/v3/5ca2c543-14b1-4984-8e3f-96d4cc23b71d';
+// this mock api url can be deleted via this url ->
+// https://designer.mocky.io/manage/delete/5ca2c543-14b1-4984-8e3f-96d4cc23b71d/codesandbox-kjaer-challenge
 
-export function fetchSandbox() {
-  const sandbox = new Promise((resolve, reject) => {
-    resolve(sandboxDummy);
-  })
+export async function fetchSandbox() {
+  const response = await fetch(MOCK_API_URL);
+  const sandbox = await response.json();
 
   return sandbox;
 }
-
-// const API_URL = "https://codesandbox.io/api/v1/sandboxes/84jkx";
-//
-// export async function fetchSandbox() {
-//   const response = await fetch(API_URL);
-//   const sandbox = await response.json();
-//
-//   return sandbox;
-// }

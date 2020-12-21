d361fb554e968799a14006173bfed66b250efded
Halil Kayer <kayer.halil@gmail.com>
2020-12-21 15:01:01 +0100

    move highlights in viewport (#4)

--- src/components/file-searcher/file-tree/FileTree.js
+++ src/components/file-searcher/file-tree/FileTree.js
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import { Fragment, useLayoutEffect, useRef, createRef } from "react";
 
 // Components
 import Status from "../status/Status";
@@ -54,6 +54,37 @@ export default function FileTree(props) {
 
 function FileNode(props) {
   const { file, file: { matches: { highlighted } } } = props
+
+  const { current: highlightsRefs } = useRef(Array.from({ length: highlighted.length }, () => createRef(null)))
+
+  function moveHighlightsInViewport(container) {
+    const containerBounds = container.getBoundingClientRect();
+    const highlightedTermBounds = container.querySelector(`mark.${styles.highlight}`).getBoundingClientRect();
+    /*
+     * values below fetching from the css file. By doing so, I will eliminate the direct coupling between FileNode
+     * component and FileTree component.
+     * Spaces below are necessary values in order to shift the highlighted value in the viewport.
+     */
+    const containerSpace = parseInt(getComputedStyle(container).getPropertyValue('--parent-space'));
+    const wrapperSpace = parseInt(getComputedStyle(container).getPropertyValue('--wrapper-space'));
+    const scrollSpace = parseInt(getComputedStyle(container).getPropertyValue('--scroll-space'));
+
+    const highlightedTermPosition = highlightedTermBounds.left + containerSpace + wrapperSpace + scrollSpace;
+
+    if ( highlightedTermPosition > containerBounds.right) {
+      const termElementLeft = Math.ceil(highlightedTermBounds.left);
+      const containerElementRight = Math.ceil(containerBounds.right);
+      const termLong = Math.ceil(highlightedTermBounds.width);
+      const spaces = containerSpace + wrapperSpace + scrollSpace;
+
+      container.style.textIndent = (((termElementLeft - containerElementRight) + termLong + spaces) * -1) + "px";
+    }
+  }
+
+  useLayoutEffect(() => {
+    highlightsRefs.forEach(({current:container}) => moveHighlightsInViewport(container))
+  }, [])
+
   return(
     <details className={styles.fileNode} open>
       <summary
@@ -70,7 +101,11 @@ function FileNode(props) {
         data-testid={`csbx-file-search-file-occurrence-list-${file.id}`}
       >
         {highlighted.map(({prefix, searchTerm, suffix}, idx) => (
-          <li key={`${file.id}-${idx}`} className={styles.line}>
+          <li
+            key={`${file.id}-${idx}`}
+            ref={highlightsRefs[idx]}
+            className={styles.line}
+          >
             {prefix}
             <mark
               className={styles.highlight}
